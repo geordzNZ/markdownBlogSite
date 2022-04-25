19ecf33624b7d6702fbf6d9df48b4527c4136e42
Geordie King <geordie.king@gmail.com>
2022-04-25 19:58:03 +0100

    create fake artilces and semi stile index.ejs

--- server.js
+++ server.js
@@ -9,9 +9,14 @@ app.use('/articles', articleRouter)
 app.get('/', (req, res) => {
 
     const articles = [{
-        title: 'Test Article',
-        createdAd: Date.now(),
-        description: 'Test Description'
+        title: 'Test Article1',
+        createdAt: new Date(),
+        description: 'Deserunt amet incididunt mollit amet nisi nostrud elit id ut sint mollit. Ut id laboris dolore sit cillum laborum ut eu aute nisi sit Lorem cillum. Laborum minim aliquip elit exercitation sunt enim minim exercitation dolor pariatur. Incididunt elit veniam laborum ullamco voluptate occaecat cupidatat eiusmod excepteur.'
+    },
+    {
+        title: 'Test Article2',
+        createdAt: new Date(),
+        description: 'Minim velit sint esse Lorem commodo aute ipsum est occaecat minim sunt. Voluptate qui id anim est aliquip excepteur pariatur ea cillum ad et culpa. Ex esse enim pariatur aute exercitation culpa consequat. Consectetur minim incididunt incididunt proident elit qui cillum ut sunt ut aliqua.'
     }]
     
     res.render('index', { articles: articles})

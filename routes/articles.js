5bf0eb10c97a6b0146ae5ebb5af9e7a75449cac4
Geordie King <geordie.king@gmail.com>
2022-04-27 22:18:59 +0100

    dealing with form submission

--- routes/articles.js
+++ routes/articles.js
@@ -1,19 +1,36 @@
 const express = require('express')
+const Article = require('./../models/article')
 const router = express.Router()
 
 
 router.get('/new', (req, res) => {
-    res.render('articles/new.ejs')
+    res.render('articles/new')
 })
 
-router.post('/', (req, res) => {
+router.get('/:id', (req, res) => {
     
 })
 
-
-
-
-
+router.post('/', async (req, res) => {
+    console.log("router.post 1");
+    const article = new Article({
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown
+    })
+    console.log("router.post 2");
+    try {
+        console.log("router.post 3");
+        article = await article.save()
+        console.log("router.post 4");
+        res.redirect(`articles/${article.id}`)
+        console.log("router.post 5");
+    } catch (e) {
+        console.log("router.post 6");
+        res.render('articles/new', { article: article })
+        console.log("router.post 7");
+    }
+})
 
 module.exports = router
 
--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
 const app = express()
 
-app.set('view engine', 'ejs')
+mongoose.connect('mongodb://localhost/blog')
 
-app.use('/articles', articleRouter)
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false}))
 
 app.get('/', (req, res) => {
 
     const articles = [{
         title: 'Test Article1',
         createdAt: new Date(),
-        description: 'Deserunt amet incididunt mollit amet nisi nostrud elit id ut sint mollit. Ut id laboris dolore sit cillum laborum ut eu aute nisi sit Lorem cillum. Laborum minim aliquip elit exercitation sunt enim minim exercitation dolor pariatur. Incididunt elit veniam laborum ullamco voluptate occaecat cupidatat eiusmod excepteur.'
+        description: 'Deserunt amet incididunt mollit amet nisi nostrud elit id ut sint mollit..'
     },
     {
         title: 'Test Article2',
         createdAt: new Date(),
-        description: 'Minim velit sint esse Lorem commodo aute ipsum est occaecat minim sunt. Voluptate qui id anim est aliquip excepteur pariatur ea cillum ad et culpa. Ex esse enim pariatur aute exercitation culpa consequat. Consectetur minim incididunt incididunt proident elit qui cillum ut sunt ut aliqua.'
+        description: 'Minim velit sint esse Lorem commodo aute ipsum est occaecat minim sunt. '
     }]
     
     res.render('articles/index', { articles: articles})
 })
 
+
+app.use('/articles', articleRouter)
 app.listen(1975)
